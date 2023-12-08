e33103b9b08de3b1d14dcbdacb4d45d6ce3079cb
Alhrafas <abbadegugger@gmail.com>
2023-12-08 15:35:41 +0100

    initial commit

--- website/views.py
+++ website/views.py
@@ -54,3 +54,16 @@ def customer_record(request, pk):
     else:
         messages.success(request, "You Must Be Logged In To View This Page....")
         return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:    
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "You Have Successfully Delete The Record...")
+        return redirect('home')    
+    else:
+        messages.success(request, "You Have To Log In To Delete The Record....")
+        return render(request, 'home.html')
+    
+def add_record(request):
+    return render(request, 'add_record.html', {})    
